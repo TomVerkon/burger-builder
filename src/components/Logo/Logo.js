6f457fd31a82394af30b4de6eda378334587fd8b
Tom Verkon <tverkon@usa.net>
2020-10-08 16:40:47 -0400

    lesson 145 - finished desktop navigation

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,18 @@
+import React from 'react'
+//import PropTypes from 'prop-types'
+import logo from '../../assets/images/burger-logo.png';
+import classes from './Logo.module.css';
+
+const Logo = props => {
+  return (
+    <div className={classes.Logo}>
+      <img src={logo} alt=''/>
+    </div>
+  )
+}
+
+// Logo.propTypes = {
+
+// }
+
+export default Logo
---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,21 @@
+import React from "react";
+//import PropTypes from "prop-types";
+import classes from "./Toolbar.module.css";
+import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
+
+const Toolbar = (props) => {
+  return (
+    <header className={classes.Toolbar}>
+      <div>MENU</div>
+      <Logo />
+      <nav>
+        <NavigationItems />
+      </nav>
+    </header>
+  );
+};
+
+//Toolbar.propTypes = {};
+
+export default Toolbar;
